9517d6b670dece697cdecb9f5b9ae2f51fd6d787
Juan Luis <jal535c@gmail.com>
2019-10-09 10:16:25 +0200

    corregida indentacion

---
+++ tema5/Ejer10.java
@@ -0,0 +1,29 @@
+/*
+*
+* Ejercicio 10: Calcula media de numeros, para detener introducir numero negativo
+*
+*/
+
+import java.util.Scanner;
+
+public class Ejer10 {
+  public static void main(String[] args) {
+    float n;        
+    float suma=0f; 
+    int i=0;        //contador, cantidad de numeros introducidos
+    Scanner s = new Scanner(System.in);
+    float media;
+
+    System.out.println("Introduzca numeros (negativo para detener): ");
+    n = s.nextFloat();
+    
+    while (n>=0) {
+      suma += n;
+      i++;
+      n = s.nextFloat();
+    }
+
+    media = suma/i;
+    System.out.printf("La media es: %.2f\n", media);
+  }
+}
