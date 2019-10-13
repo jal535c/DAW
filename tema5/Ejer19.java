add740625789ff9d413b3253e6a4779e516e20b1
Juan Luis <jal535c@gmail.com>
2019-10-13 14:16:22 +0200

    mejora

--- tema5/Ejer19.java
+++ tema5/Ejer19.java
@@ -1,6 +1,6 @@
 /*
 *
-* Ejercicio 19: Piramide, se pide por teclado altura y caracter
+* Ejercicio 19: Piramide (se pide por teclado altura y caracter)
 *
 */
 
@@ -13,19 +13,23 @@ public class Ejer19 {
     Scanner s = new Scanner(System.in);
 
     System.out.print("Introduzca altura: ");
-    n = Integer.parseInt(System.console().readLine());
+    n = s.nextInt();
 
     System.out.print("Introduzca caracter: ");
     c = s.next().charAt(0);
 
-    for (int j=0; j<n; j++) {
-      for (int i=0; i<n-j; i++) {
+    for (int fila=0; fila<n; fila++) {
+      
+      for (int i=0; i<n-fila; i++) {
         System.out.print(" ");
       }
-      for (int i=n-j; i<=n; i++) {
+      
+      for (int i=n-fila; i<=n; i++) {
         System.out.printf("%c ", c);
       }
+            
       System.out.printf("\n");
+    
     }
   }
 }
