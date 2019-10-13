78f552e3c6a9acbbcd50b7c1cf14facfcb6901d0
Juan Luis <jal535c@gmail.com>
2019-10-13 20:01:20 +0200

    primer commit

---
+++ tema5/Ejer04.java
@@ -0,0 +1,17 @@
+/*
+* 
+* Ejercicio 4: Bucle for desde 320 a 160 (de 20 en 20)
+*  
+*/
+
+public class Ejer04 {
+  public static void main(String[] args) {
+    int n;
+    
+    for (n=320; n>=160; n-=20) {
+      System.out.printf("%d ", n);
+    }
+    
+    System.out.println();
+  }
+}
--- tema5/Ejer05.java
+++ tema5/Ejer05.java
@@ -1,17 +1,17 @@
 /*
 * 
-* Ejercicio 6: Bucle do-while desde 320 a 160 (de 20 en 20)
+* Ejercicio 5: Bucle while desde 320 a 160 (de 20 en 20)
 *  
 */
 
-public class Ejer06 {
+public class Ejer05 {
   public static void main(String[] args) {
     int n = 320;
     
-    do {
+    while (n>=160) {
       System.out.printf("%d ",n);
       n -= 20;
-    } while (n>=160);
+    }
     
     System.out.println();
   }
