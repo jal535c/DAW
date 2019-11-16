c5f6bcf4d499f6026fd61b4aabcd890ac070a7f4
Juan Luis <jal535c@gmail.com>
2019-11-16 16:47:41 +0100

    reverso de un numero

--- tema5/Ejer25.java
+++ tema5/Ejer25.java
@@ -6,15 +6,17 @@
 
 public class Ejer25 {
   public static void main(String[] args) {
-    int n, digit, resul=0;
+    int n;
+    int digit;
+    int resul=0;
 
     System.out.print("Introduzca numero: ");
     n = Integer.parseInt(System.console().readLine());
  
     while (n>0) {
-      digit = n % 10;               //cojo el ultimo
-      resul = digit + (resul*10);   //acumulo con lo que tenia
-      n = n/10;                     //actualiza numero
+      digit = n % 10;               //cojo el ultimo digito
+      resul = (resul*10) + digit;   //acumulo con lo que tenia
+      n = n/10;                     //actualiza numero (recorto)
     }
 
     System.out.println("Reverso: " + resul);
