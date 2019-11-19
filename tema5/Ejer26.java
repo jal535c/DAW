92d93ddd51120fe6c72be5715949465cba170bf2
Juan Luis <jal535c@gmail.com>
2019-11-19 19:38:31 +0100

    posiones de un digito en un numero

--- tema5/Ejer26.java
+++ tema5/Ejer26.java
@@ -1,6 +1,6 @@
 /*
 *
-* Ejercicio 26: Calcula posiciones que un digito aparece en un numero
+* Ejercicio 26: Calcula las posiciones en que un digito aparece en un numero (de izquierda a derecha)
 *
 */
 
@@ -8,7 +8,7 @@ public class Ejer26 {
   public static void main(String[] args) {
     int n;
     int dig;
-
+    int resto;
     int reverso = 0;
     int tam = 0;
     int pos = 1;
@@ -22,13 +22,14 @@ public class Ejer26 {
     
     System.out.println("El digito aparece en las posiciones:");
     
-    while (n > 0) {     //invierte el numero y calcula su tamaño
-      reverso = (reverso * 10) + (n % 10);
+    while (n > 0) {     //invierte el numero (para hacer el calculo desde la derecha)
+      resto = n%10;
+      reverso = (reverso*10) + resto;
       n /= 10;
-      tam++;
+      tam++;        //el tamaño no es necesario
     }
        
-    while (reverso > 0) {     //calcula posicion
+    while (reverso > 0) {     //va recortanto el numero para comparar con dig
       if ((reverso % 10) == dig) {
         System.out.print(pos + " ");
       }
