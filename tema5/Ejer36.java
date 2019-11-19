9077e225476ce53e1440607d1dd280509c9de103
Juan Luis <jal535c@gmail.com>
2019-11-19 18:17:33 +0100

    capicua de cualquier longitud

--- tema5/Ejer36.java
+++ tema5/Ejer36.java
@@ -6,16 +6,17 @@
 
 public class Ejer36 {
   public static void main(String[] args) {
-    long n, original, digit, reves=0;
+    long n, original, reves=0;
+    int digit;
 
     System.out.print("Introduzca numero: ");
     n = Long.parseLong(System.console().readLine());
+    
     original = n;
- 
     while (n>0) {
-      digit = n % 10;               
-      reves = digit + (reves*10);   
-      n = n/10;                     
+      digit = (int)(n % 10);      //coge digito de la derecha
+      reves = (reves*10)+digit;   //va creando el nuevo numero
+      n = n/10;                   //recorta el numero original
     }
 
     if (original == reves) {
