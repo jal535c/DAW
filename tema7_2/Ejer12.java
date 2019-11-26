32d7abe92c9439b679384a65abcadb9cc4b001c4
Juan Luis <jal535c@gmail.com>
2019-11-26 20:04:42 +0100

    max, min, media de la otra diagonal

---
+++ tema7_2/Ejer12.java
@@ -0,0 +1,52 @@
+/*
+*
+* Ejercicio 12: Max, min, media de la otra diagonal de matriz
+*
+*/
+
+public class Ejer12 {
+  public static void main(String[] args) {
+    int[][] arr = new int[9][9];
+    int max=500;
+    int min=900;
+    int suma=0;
+    double media;
+    int[] aux = new int[9];
+
+    for (int i=0; i<9; i++) {
+      for (int j=0; j<9; j++) {
+        arr[i][j] = (int)(Math.random()*401)+500;
+        if (i+j==8) {
+          aux[i]=arr[i][j];     //diagonal
+
+          suma+=aux[i];         //suma acumulada
+          
+          if (aux[i]>max) {     //maximo
+            max=aux[i];
+          }
+          if (aux[i]<min) {     //minimo
+            min=aux[i];
+          }
+        }
+      }
+    } 
+
+    for (int i=0; i<9; i++) {
+      for (int j=0; j<9; j++) {
+        System.out.printf("%4d", arr[i][j]);
+      }
+      System.out.println();
+    }
+
+    System.out.println();
+
+    for (int i=0; i<9; i++) {
+      System.out.printf("%d ", aux[i]);
+    }
+
+    media=(double)suma/9;
+    System.out.printf("\nMAX: %d\n", max);
+    System.out.printf("MIN: %d\n", min);
+    System.out.printf("Media: %.2f\n", media);
+  }
+}
