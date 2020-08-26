228430114b97ac31e253f5ab72cc707dc2087921
Juan Luis <jal535c@gmail.com>
2020-08-26 23:55:25 +0200

    ficheros de texto

--- tema11/Ejer2.java
+++ tema11/Ejer2.java
@@ -3,18 +3,18 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Ejer2 {
-	public static void main(String[] args) {
-		String linea="";
-		try {
-			BufferedReader br = new BufferedReader(new FileReader("primos.dat"));
-			do {
-				linea = br.readLine();
-				if (linea!=null) {			//para que no imprima null al final
-					System.out.println(linea);	
-				}				
-			} while (linea!=null);
-		} catch (IOException ioe) {
-			System.out.print("error");
-		}
-	}
+  public static void main(String[] args) {
+    String linea = "";
+    try {
+      BufferedReader br = new BufferedReader(new FileReader("primos.dat"));
+      do {
+        linea = br.readLine();
+        if (linea != null) { // para que no imprima null al final
+          System.out.println(linea);
+        }
+      } while (linea != null);
+    } catch (IOException ioe) {
+      System.out.print("error");
+    }
+  }
 }
