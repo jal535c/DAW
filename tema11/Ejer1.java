699100e9cc654968611f29b36c017565060b0638
Juan Luis <jal535c@gmail.com>
2020-08-26 23:53:20 +0200

    ficheros de texto

--- tema11/Ejer1.java
+++ tema11/Ejer1.java
@@ -3,28 +3,28 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Ejer1 {
-	public static void main(String[] args) {
-		boolean primo=true;
+  public static void main(String[] args) {
+    boolean primo = true;
 
-		try {
-			BufferedWriter bw = new BufferedWriter(new FileWriter("primos.dat"));
+    try {
+      BufferedWriter bw = new BufferedWriter(new FileWriter("primos.dat"));
 
-			for (int i=2; i<500; i++) {
-				primo=true;
-				for (int j=2; j<i && primo==true; j++) {
-					if (i%j==0) {
-						primo=false;
-					}
-				}
+      for (int i=2; i<500; i++) {
+        primo = true;
+        for (int j=2; j<i && primo==true; j++) {
+          if (i%j == 0) {
+            primo = false;
+          }
+        }
 
-				if (primo==true) {
-					bw.write(i+" ");
-				}
-			}
-			
-			bw.close();
-		} catch (IOException ioe) {
-			System.out.println("error");
-		}
-	}
+        if (primo == true) {
+          bw.write(i + " ");
+        }
+      }
+
+      bw.close();
+    } catch (IOException ioe) {
+      System.out.println("error");
+    }
+  }
 }
